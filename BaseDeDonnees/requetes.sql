1953b821a8bad4c4ad579e980040aba274e6b972
Gouga34 <imala34@hotmail.fr>
2014-12-04 10:16:45 +0100

    Ajout des requêtes manquantes + rectifications de noms d'attributs dans le diagramme de classe

--- BaseDeDonnees/requetes.sql
+++ BaseDeDonnees/requetes.sql
@@ -16,7 +16,7 @@ set linesize 250;
 
 
 
-/***REQUETES OK ********************************************************************************************************************/
+/***REQUETES ********************************************************************************************************************/
 
 /*creation utilisateur*/ 
     INSERT INTO Utilisateur (login, mdp, mail, admin) VALUES ('$log','$psw','$email','$admn');
@@ -30,13 +30,16 @@ set linesize 250;
 /*creation termeVedette*/
     INSERT INTO termeVedette (idTerme, nomTerme, description) VALUES(0,'Gobou','Gobou est un pokemon eau mignon <3');
     INSERT INTO termeVedette (idTerme, nomTerme, description) VALUES(1,'Entei','Best woofi ever ! <3');
+    insert into termeVedette(idTerme,nomTerme,description) VALUES(2,'Dresseurs','les dresseurs pokemon');
 
 /*creation synonyme*/
     INSERT INTO Synonyme (idSynonyme, nomSynonyme) VALUES (0,'Boubou');
+    INSERT INTO Synonyme (idSynonyme, nomSynonyme) VALUES (1'Waf');
 
 /*creation concept*/
     INSERT INTO Concept (idConcept, nomConcept, description, vedette) VALUES (0, 'Pokemon', 'Un concept pokemon', (select REF(T) from TermeVedette T where idTerme = 0));
     INSERT INTO Concept (idConcept, nomConcept, description, vedette) VALUES (1, 'Pokemon2', 'Un concept pokemon 2', (select REF(T) from TermeVedette T where idTerme = 1));
+    INSERT INTO Concept (idConcept, nomConcept, description, vedette) VALUES (2, 'Dresseurs', 'Les dresseurs du dessin animé', (select REF(T) from TermeVedette T where idTerme = 2));
     /* Afficher le terme associé au concept */ select c.vedette.nomTerme from Concept c where c.idConcept=0;
     
     /*modification du concept*/
@@ -46,19 +49,58 @@ set linesize 250;
 
 /*Lier un concept à son possesseur*/
 
-/* on doit commencer par faire un update pour la première valeur, des insert pour les suivantes */
-
-    UPDATE Utilisateur
-       set concepts = GroupeConcept_t((SELECT ref(c) FROM Concept c WHERE c.idConcept = 0))
-     where login = 'Manu';  
+	/* on doit commencer par faire un update pour la première valeur, des insert pour les suivantes */
+	    UPDATE Utilisateur
+	       set concepts = GroupeConcept_t((SELECT ref(c) FROM Concept c WHERE c.idConcept = 0))
+	     where login = 'Manu';  
     
-    INSERT INTO TABLE (SELECT concepts FROM Utilisateur WHERE login = 'Manu')
-    VALUES ((SELECT ref(c) FROM Concept c WHERE c.idConcept = 1));
+	    INSERT INTO TABLE (SELECT concepts FROM Utilisateur WHERE login = 'Manu')
+	    VALUES ((SELECT ref(c) FROM Concept c WHERE c.idConcept = 1));
 
     
     select nomConcept from
     (select u.concepts from Utilisateur u where u.login = 'Manu');
+
+/*Lier un synonyme à son possesseur*/
+    UPDATE Utilisateur
+       set synonymes = GroupeSynonyme_t((SELECT ref(s) FROM Synonyme s WHERE s.idSynonyme = 0))
+     where login = 'Manu';  
+    
+    INSERT INTO TABLE (SELECT Synonymes FROM Utilisateur WHERE login = 'Manu')
+    VALUES ((SELECT ref(s) FROM Synonyme s WHERE s.idSynonyme = 1));
       
+/*Lier un terme à son possesseur*/
+	UPDATE Utilisateur
+	   set termes = GroupeTerme_t((SELECT ref(t) FROM TermeVedette t WHERE t.idTerme = 0))
+	 where login = 'Manu'; 
+	    
+	INSERT INTO TABLE (SELECT termes FROM Utilisateur WHERE login = 'Manu')
+	VALUES ((SELECT ref(t) FROM TermeVedette t WHERE t.idTerme = 1));
+
+/*Lier un synonyme à son terme*/
+	UPDATE TermeVedette
+	   set synonymes = GroupeSynonyme_t((SELECT ref(s) FROM Synonyme s WHERE s.idSynonyme = 0))
+	 where idTerme = 0;
+	    
+	INSERT INTO TABLE (SELECT synonymes FROM TermeVedette WHERE idTerme = 0)
+	VALUES ((SELECT ref(s) FROM Synonyme s WHERE s.idSynonyme = 1));
+
+/*Ajouter un parent à un concept*/
+	UPDATE Concept
+	   set parents = GroupeConcept_t((SELECT ref(c) FROM Concept c WHERE c.idConcept = 1))
+	 where idConcept = 2;
+	    
+	INSERT INTO TABLE (SELECT parents FROM Concept WHERE idConcept = 2)
+	VALUES ((SELECT ref(c) FROM Concept c WHERE c.idConcept = 0));
+
+/* Lier un concept à son fils */
+
+	UPDATE Concept
+	   set fils = GroupeConcept_t((SELECT ref(c) FROM Concept c WHERE c.idConcept = 1))
+	 where idConcept = 0;
+	    
+	INSERT INTO TABLE (SELECT fils FROM Concept WHERE idConcept = 0)
+	VALUES ((SELECT ref(c) FROM Concept c WHERE c.idConcept = 0));
     
 /*modif utilisateur(mdp)*/
     UPDATE Utilisateur SET mdp='53' WHERE login='Manu';
@@ -85,167 +127,27 @@ set linesize 250;
 /* Supprimer utilisateur */
     DELETE FROM utilisateur WHERE login = 'Manu';
 
+/*Récupérerla vedette d'un concept*/
+	select deref(vedette) from concept where idConcept=0;
 
-/**REQUETES PAS ENCORE OK**********************************************************************************************************/
+/*avoir liste synonyme appartenant à un utilisateur*/  
+	select deref(value(listeSynonymes)) from utilisateur u, table(u.synonymes) listeSynonymes;
 
+/*avoir liste synonymes d'un terme*/
+	SELECT deref(VALUE(listeSynonymes))
+	FROM TermeVedette t, TABLE(t.synonymes) listeSynonymes
+	WHERE t.idTerme = 0;
 
-/*ajout de concept à un utilisateur*/
+/*avoir liste termes appartenant à un utilisateur*/
+	select deref(value(listeTermes)) from utilisateur u, table(u.termes) listeTermes;
 
-
-/*Lier un synonyme à son terme*/
-
-
-/*Lier un synonyme à son possesseur*/
-
-
-/*Lier un terme à son possesseur*/
-
-/*avoir liste synonymes d'un terme*/ /*A tester quand on aura "lier un synonyme à son terme"*/
-
-SELECT t.synonymes
-FROM TermeVedette t
-WHERE t.nomTerme = '$nomTerme';
-
-/*avoir liste termes appartenant à un utilisateur*/ /*A tester quand on aura "lier un terme à son possesseur"*/
-
-SELECT U.termes
-FROM Utilisateur U
-WHERE U.login='$log';
-
-/*avoir liste synonyme appartenant à un utilisateur*/  /*A tester quand on aura "lier un synonyme à son possesseur"*/
-
-SELECT U.synonymes
-FROM Utilisateur U
-WHERE U.login='$log';
+/*avoir les parents d'un concept*/
+	select deref(value(listeParents)) from concept c, table(c.parents) listeParents;
 
 /*avoir les fils d'un concept*/
-
-SELECT c.fils[1].nomConcept from Concept c where c.idConcept=0; //geoffrey je rage ici
-
-select c.nomConcept from THE (select u.concepts from utilisateur u where login = 'Manu') c;
-
-SELECT e.nom FROM THE ( SELECT employes
-FROM departements WHERE numdep = 1 ) e ;
+	SELECT deref(VALUE(listeFils)) FROM Concept c, TABLE(c.fils) listeFils WHERE c.idConcept = 0;
 
 
-/**CODE ACTUEL DE LA BD*********************************************************************************************************/
-
-
-
-drop table Utilisateur;
-drop table Concept;
-drop table TermeVedette;
-drop table Synonyme;
-drop type Utilisateur_t;
-drop type Concept_t force;
-drop type GroupeTerme_t;
-drop type TermeVedette_t;
-drop type GroupeConcept_t;
-drop type GroupeSynonyme_t;
-drop type Synonyme_t;
-
-
-
-/*Création des types*********************************************************************************************/
-
-create or replace type Synonyme_t as Object
-(
-	idSynonyme int,
-	nomSynonyme VARCHAR(30)
-);
-/
-
-create type Concept_t;
-/
-
-create or replace type GroupeSynonyme_t as table of ref Synonyme_t;
-/
-
-create or replace type TermeVedette_t as Object
-(
-	idTerme int,
-	nomTerme VARCHAR(30),
-	description VARCHAR(200),
-	synonymes GroupeSynonyme_t
-);
-/
-
-
-create or replace type GroupeTerme_t as table of ref TermeVedette_t;
-/
-
-create  type GroupeConcept_t as table of ref Concept_t;
-/
-
-
-create or replace type Concept_t as Object
-(
-	idConcept int,
-	nomConcept VARCHAR(30),
-	description VARCHAR(200),
-	fils GroupeConcept_t,
-	parents GroupeConcept_t,
-	vedette REF TermeVedette_t
-);
-/
-
-create or replace type Utilisateur_t as Object /*type utilisateur surement inutile*/
-(
-	login VARCHAR(30),
-	mdp VARCHAR(100),
-	mail VARCHAR(50),
-	admin number(1), /*0 : utilisateur 1 : admin*/
-	concepts GroupeConcept_t,
-	synonymes GroupeSynonyme_t,
-	termes GroupeTerme_t
-
-);
-/
-
-
-
-/*Création des tables***************************************************************************************/
-
-create table Synonyme of Synonyme_t
-(
-	constraint cp_synonyme primary key (idSynonyme),
-	constraint notN_nomSynonyme check (nomSynonyme IS NOT NULL)
-);
-
-create table TermeVedette of TermeVedette_t
-(
-	constraint cp_termeVedette primary key (idTerme),
-	constraint notN_nomTerme check (nomTerme IS NOT NULL)
-)
-nested table synonymes store as listeSynonymesTerme;
-
-create table Concept of Concept_t
-(
-	constraint cp_concept primary key (idConcept),
-	/*constraint autoin_concept AUTO_INCREMENT (idConcept),*/
-	constraint notN_nomConcept check (nomConcept IS NOT NULL),
-	constraint notN_description check (description IS NOT NULL),
-	constraint notN_vedette check (vedette IS NOT NULL)
-)
-nested table fils store as listeFilsConcept,
-nested table parents store as listeParentsConcept
-;
-
-create table Utilisateur of Utilisateur_t
-(
-	constraint cp_utilisateur primary key(login),
-	constraint notN_mdp check (mdp IS NOT NULL),
-	constraint notN_mail check (mail IS NOT NULL),
-	constraint notN_admin check (admin IS NOT NULL),
-	constraint unic_mail unique (mail),
-	constraint forme_mail check (mail LIKE '%@%.%'),
-	constraint value_admin check (admin BETWEEN 0 and 1),
-	constraint default_admin admin DEFAULT (0)
-)
-nested table concepts store as listeConceptsUtilisateur,
-nested table synonymes store as listeSynonymesUtilisateur,
-nested table termes store as listeTermesUtilisateur 
-;
 
 
 
